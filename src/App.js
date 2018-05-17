03e4f643ce0405706f72712eb0f67ce7560a879a
Andrei Hensialevich <sixpackgods@gmail.com>
2018-05-17 14:08:23 +0200

    Added search component (styling only)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Classes from './App.scss'
 import Main from './Containers/Main/Main';
 
 class App extends Component {
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import * as classes from './Search.scss';
 
 const search = props => (
-    <div>
-        <input type='text' />
+    <div className={classes.Search_cont}>
+        <input className={classes.Search_bar} placeholder='Input city here...' type='text' />
+        <button className={classes.Add_button} type='button'><i className="fas fa-plus"></i></button>
     </div>
 );
 
